cfc20b70cc1d9e338b754b93885d4af0c24adee5
Dev1000 <javaimagination@gmail.com>
2024-05-23 00:16:59 -0500

    Connect o Render Postgres

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [posts, setPosts] = useState([])
+
+  const getPosts = () => {
+    axios.get(`${import.meta.env.VITE_API_URL}/posts/index`)
+    .then(r => {
+      console.log('r.data:',r.data)
+      setPosts(r.data.posts)
+    }).catch(e => console.log(e))
+  }
 
   return (
     <>
@@ -24,6 +34,10 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <button onClick={getPosts}>Get Posts</button>
+        <ul>
+          {posts.map(post => <li key={post.id}>{post.title}</li>)}
+        </ul>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
